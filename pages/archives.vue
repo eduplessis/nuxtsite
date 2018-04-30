9a935b693c4310ff7df2896f2d54c63080086d46
Edouard Duplessis <eduplessis@gmail.com>
2018-04-30 10:16:26 -0400

    test netlify nuxt

---
+++ pages/archives.vue
@@ -0,0 +1,31 @@
+<template>
+
+<div>
+
+  <h1> All Posts </h1>
+
+  <ul>
+
+    <li v-for="post in posts" :key="post.title">
+
+      <nuxt-link :to="post.permalink">{{ post.title }}</nuxt-link>
+
+    </li>
+
+  </ul>
+
+</div>
+
+</template>
+
+
+
+<script>
+export default {
+  async asyncData({ app }) {
+    return {
+      posts: await app.$content("/").getAll()
+    };
+  }
+};
+</script>
